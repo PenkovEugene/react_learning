3a778704b4cdf0ce3461e887a1d6f46ea7b4d73e
PenkovEugene <penkovzheny@gmail.com>
2023-10-18 22:06:27 +0500

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/App.css 	deleted:    src/App.test.tsx 	modified:   src/App.tsx 	new file:   src/components/CreateProduct.tsx 	new file:   src/components/ErrorMessage.tsx 	new file:   src/components/Loader.tsx 	new file:   src/components/Modal.tsx 	new file:   src/components/Product.tsx 	new file:   src/context/ModalContext.tsx 	new file:   src/data/products.ts 	new file:   src/hooks/products.ts 	modified:   src/index.css 	modified:   src/index.tsx 	new file:   src/jsx.example.tsx 	deleted:    src/logo.svg 	new file:   src/models.ts 	deleted:    src/reportWebVitals.ts 	new file:   tailwind.config.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Loader } from './components/Loader';
+import { ErrorMessage } from './components/ErrorMessage';
+import {Product} from './components/Product'
+import { useProducts } from './hooks/products';
+import { Modal } from './components/Modal';
+import { CreateProduct } from './components/CreateProduct';
+import { useContext } from 'react';
+import { IProduct } from './models';
+import { ModalContext } from './context/ModalContext';
 
 function App() {
+  const {loading, error, products, addProduct} = useProducts()
+  const {modal, open, close} = useContext(ModalContext)
+
+
+  const createHandler = (product: IProduct) => {
+    close()
+    addProduct(product)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto max-w-2xl pt-5">
+      {loading && <Loader />}
+      {error && <ErrorMessage error={error} />}
+      {products.map(product => <Product product={product} key={product.id}/>)}
+
+      {modal && <Modal title='Create new product' onClose={close}>
+        <CreateProduct onCreate={createHandler}/>
+      </Modal>}
+
+      <button 
+        className='fixed bottom-5 right-5 rounded-full bg-red-700 text-white py-2 px-4'
+        onClick={open}
+      >Add product</button>
     </div>
-  );
+    )
 }
 
 export default App;
